6ac52dbe70c0dc26ff31f79dd074b937f5387f95
Olle Karlson Reutercrona <ollekreutercrona@gmail.com>
2025-08-31 18:58:52 +0200

    Fixed tests and cleaned up m3x3 class

--- Source/Core/Math/Matrix3x3.h
+++ Source/Core/Math/Matrix3x3.h
@@ -11,9 +11,15 @@ public:
 	Matrix3x3(Matrix3x3& other) : mMatrix(other) {}
 	Matrix3x3(const DirectX::XMMATRIX& other) : mMatrix(other) {}
 	Matrix3x3(DirectX::XMMATRIX& other) : mMatrix(other) {}
+	Matrix3x3(const float* floatArray) : mMatrix(floatArray) {}
+	Matrix3x3(
+		const float r0c0, const float r0c1, const float r0c2,
+		const float r1c0, const float r1c1, const float r1c2,
+		const float r2c0, const float r2c1, const float r2c2);
 
 	float& operator()(const unsigned int row, const unsigned int column) noexcept;
 	Matrix3x3& operator=(const Matrix3x3& other) noexcept;
+	Matrix3x3& operator=(const float* floatArray) noexcept;
 	Matrix3x3 operator*(const Matrix3x3& other) const noexcept;
 	Matrix3x3& operator*=(const Matrix3x3& other) noexcept;
 	Matrix3x3 operator+(const Matrix3x3& other) const noexcept;
@@ -28,9 +34,9 @@ public:
 
 	Matrix3x3 Transpose() const;
 	Matrix3x3 Inverse() const; // Todo, add parameter for determinant when Vector3 is implemented
-	Matrix3x3 CreateRotationAroundX(const float angle) const;
-	Matrix3x3 CreateRotationAroundY(const float angle) const;
-	Matrix3x3 CreateRotationAroundZ(const float angle) const;
+	static inline Matrix3x3 CreateRotationAroundX(const float angle);
+	static inline Matrix3x3 CreateRotationAroundY(const float angle);
+	static inline Matrix3x3 CreateRotationAroundZ(const float angle);
 
 	// Todo, implement these methods when Vector3 is implemented
 	//Matrix3x3 CreateTranslationMatrix(const Vector3& scaleVector);
@@ -42,6 +48,16 @@ public:
 
 static inline Matrix3x3 zerMatrix = Matrix3x3();
 
+inline Matrix3x3::Matrix3x3(
+	const float r0c0, const float r0c1, const float r0c2, 
+	const float r1c0, const float r1c1, const float r1c2, 
+	const float r2c0, const float r2c1, const float r2c2) :
+	mMatrix({
+		r0c0, r0c1, r0c2, 0.0f,
+		r1c0, r1c1, r1c2, 0.0f,
+		r2c0, r2c1, r2c2, 0.0f,
+		0.0f, 0.0f, 0.0f, 1.0f }) { }
+
 float& Matrix3x3::operator()(const unsigned int row, const unsigned int column) noexcept {
 	assert(row < 3 && column < 3 && "Matrix3x3: Index out of bounds");
 
@@ -52,6 +68,14 @@ Matrix3x3& Matrix3x3::operator=(const Matrix3x3& other) noexcept {
 	if(this != &other) {
 		mMatrix = other.mMatrix;
 	}
+
+	return *this;
+}
+
+inline Matrix3x3& Matrix3x3::operator=(const float* floatArray) noexcept {
+	assert(floatArray != nullptr && "Matrix3x3: Null pointer exception");
+
+	mMatrix = DirectX::XMMATRIX(floatArray);
 
 	return *this;
 }
@@ -109,14 +133,14 @@ Matrix3x3 Matrix3x3::Inverse() const {
 	return DirectX::XMMatrixInverse(nullptr, mMatrix);
 }
 
-Matrix3x3 Matrix3x3::CreateRotationAroundX(const float angle) const {
+Matrix3x3 Matrix3x3::CreateRotationAroundX(const float angle) {
 	return DirectX::XMMatrixRotationX(angle);
 }
 
-Matrix3x3 Matrix3x3::CreateRotationAroundY(const float angle) const {
+Matrix3x3 Matrix3x3::CreateRotationAroundY(const float angle) {
 	return DirectX::XMMatrixRotationY(angle);
 }
 
-Matrix3x3 Matrix3x3::CreateRotationAroundZ(const float angle) const {
+Matrix3x3 Matrix3x3::CreateRotationAroundZ(const float angle) {
 	return DirectX::XMMatrixRotationZ(angle);
 }
--- Source/CoreTests/Math/Matrix3x3Tests.cpp
+++ Source/CoreTests/Math/Matrix3x3Tests.cpp
@@ -1,7 +1,7 @@
 #include <gtest/gtest.h>
 
 #include <DirectXMath.h>
-#include "Matrix3x3.h"
+#include "Math/Matrix3x3.h"
 #include "Utility/TestUtility.h"
 
 using namespace DirectX;
@@ -54,7 +54,9 @@ TEST(Matrix3x3Tests, CopyContructor) {
 
 		Matrix3x3 matrix1;
 		matrix1(0, 0) = randomValue;
+		matrix1(0, 1) = randomValue;
 		matrix1(1, 1) = randomValue;
+		matrix1(1, 2) = randomValue;
 		matrix1(2, 2) = randomValue;
 
 		Matrix3x3 matrix2(matrix1);
@@ -67,30 +69,229 @@ TEST(Matrix3x3Tests, CopyContructor) {
 
 //***********************************************************************
 TEST(Matrix3x3Tests, AssignmentOperator) {
+	float randomValue = testUtility.GetRandomFloat(1.5f, 10000.0f);
+
+	auto row = testUtility.GetRandomInt(0, 3);
+	auto column = testUtility.GetRandomInt(0, 3);
+
 	Matrix3x3 matrix1;
+	matrix1(row, column) = randomValue;
+
 	Matrix3x3 matrix2;
+	EXPECT_NE(matrix2(row, column), randomValue);
+
 	matrix2 = matrix1;
 	EXPECT_EQ(matrix2.mMatrix, matrix1.mMatrix);
-
-	Matrix3x3 matrix3;
-	matrix3 = XMMatrixIdentity();
-	EXPECT_EQ(matrix3, XMMatrixIdentity());
 }
 
 //***********************************************************************
 TEST(Matrix3x3Tests, MultiplicationOperator) {
-	Matrix3x3 matrix1;
-	Matrix3x3 matrix2;
-	Matrix3x3 result = matrix1 * matrix2;
-	EXPECT_EQ(result.mMatrix, matrix1.mMatrix * matrix2.mMatrix);
+	const float r1 = testUtility.GetRandomFloat(-1000.0f, 1000.0f);
+	Matrix3x3 randomMatrix1 = {
+		r1, r1, r1,
+		r1, r1, r1,
+		r1, r1, r1,
+	};
+
+	const float r2 = testUtility.GetRandomFloat(-1000.0f, 1000.0f);
+	Matrix3x3 randomMatrix2 = {
+		r2, r2, r2,
+		r2, r2, r2,
+		r2, r2, r2,
+	};
+
+	auto resultMatrix = randomMatrix1 * randomMatrix2;
+
+	//	| a | b | c |	| j | k | l |	| aj + bm + cp | ak + bn + cq | al + bo + cr |
+	//	|---+---+---|	|---+---+---|	|--------------+--------------+--------------|
+	//	| d | e | f | X | m | n | o | = | dj + em + fp | dk + en + fq | dl + eo + fr |
+	//	|---+---+---|	|---+---+---|	|--------------+--------------+--------------|
+	//	| g | h | i |	| p | q | r |	| gj + hm + ip | gk + hn + iq | gl + ho + ir |
+
+
+	// row 0
+	{
+		// r0c0
+		const float expR0C0 =
+			randomMatrix1(0, 0) * randomMatrix2(0, 0) + // a * j
+			randomMatrix1(0, 1) * randomMatrix2(1, 0) + // b * m
+			randomMatrix1(0, 2) * randomMatrix2(2, 0);  // c * p
+
+		EXPECT_NEAR(resultMatrix(0, 0), expR0C0, gFloatMargin);
+
+		// r0c1
+		const float expR0C1 =
+			randomMatrix1(0, 0) * randomMatrix2(0, 1) + // a * k
+			randomMatrix1(0, 1) * randomMatrix2(1, 1) + // b * n
+			randomMatrix1(0, 2) * randomMatrix2(2, 1);  // c * q
+
+		EXPECT_NEAR(resultMatrix(0, 1), expR0C1, gFloatMargin);
+
+		// r0c2
+		const float expR0C2 =
+			randomMatrix1(0, 0) * randomMatrix2(0, 2) + // a * l
+			randomMatrix1(0, 1) * randomMatrix2(1, 2) + // b * o
+			randomMatrix1(0, 2) * randomMatrix2(2, 2);  // c * r
+
+		EXPECT_NEAR(resultMatrix(0, 2), expR0C2, gFloatMargin);
+	}
+
+	// row 1
+	{
+		// r1c0
+		const float expR1C0 =
+			randomMatrix1(1, 0) * randomMatrix2(0, 0) + // d * j
+			randomMatrix1(1, 1) * randomMatrix2(1, 0) + // e * m
+			randomMatrix1(1, 2) * randomMatrix2(2, 0);  // f * p
+			
+		EXPECT_NEAR(resultMatrix(1, 0), expR1C0, gFloatMargin);
+
+		// r1c1
+		const float expR1C1 =
+			randomMatrix1(1, 0) * randomMatrix2(0, 1) + // d * k
+			randomMatrix1(1, 1) * randomMatrix2(1, 1) + // e * n
+			randomMatrix1(1, 2) * randomMatrix2(2, 1);  // f * q
+
+		EXPECT_NEAR(resultMatrix(1, 1), expR1C1, gFloatMargin);
+
+		// r1c2
+		const float expR1C2 =
+			randomMatrix1(1, 0) * randomMatrix2(0, 2) + // d * l
+			randomMatrix1(1, 1) * randomMatrix2(1, 2) + // e * o
+			randomMatrix1(1, 2) * randomMatrix2(2, 2);  // f * r
+
+		EXPECT_NEAR(resultMatrix(1, 2), expR1C2, gFloatMargin);
+	}
+
+	// row 2
+	{
+		// r2c0
+		const float expR2C0 =
+			randomMatrix1(2, 0) * randomMatrix2(0, 0) + // g * j
+			randomMatrix1(2, 1) * randomMatrix2(1, 0) + // h * m
+			randomMatrix1(2, 2) * randomMatrix2(2, 0);  // i * p
+
+		EXPECT_NEAR(resultMatrix(2, 0), expR2C0, gFloatMargin);
+
+		// r2c1
+		const float expR2C1 =
+			randomMatrix1(2, 0) * randomMatrix2(0, 1) + // g * k
+			randomMatrix1(2, 1) * randomMatrix2(1, 1) + // h * n
+			randomMatrix1(2, 2) * randomMatrix2(2, 1);  // i * q
+
+		EXPECT_NEAR(resultMatrix(2, 1), expR2C1, gFloatMargin);
+
+		// r2c2
+		const float expR2C2 =
+			randomMatrix1(2, 0) * randomMatrix2(0, 2) + // g * l
+			randomMatrix1(2, 1) * randomMatrix2(1, 2) + // h * o
+			randomMatrix1(2, 2) * randomMatrix2(2, 2);  // i * r
+
+		EXPECT_NEAR(resultMatrix(2, 2), expR2C2, gFloatMargin);
+	}
 }
 
 //***********************************************************************
 TEST(Matrix3x3Tests, MultiplicationAssignmentOperator) {
-	Matrix3x3 matrix1;
-	Matrix3x3 matrix2;
-	matrix1 *= matrix2;
-	EXPECT_EQ(matrix1.mMatrix, XMMatrixIdentity());
+	const float r1 = testUtility.GetRandomFloat(-1000.0f, 1000.0f);
+	Matrix3x3 randomMatrix1 = {
+		r1, r1, r1,
+		r1, r1, r1,
+		r1, r1, r1,
+	};
+
+	const float r2 = testUtility.GetRandomFloat(-1000.0f, 1000.0f);
+	Matrix3x3 randomMatrix2 = {
+		r2, r2, r2,
+		r2, r2, r2,
+		r2, r2, r2,
+	};
+
+	Matrix3x3 originalMatrix1 = randomMatrix1;
+	ASSERT_EQ(randomMatrix1, originalMatrix1);
+
+	randomMatrix1 *= randomMatrix2;
+
+	// row 0
+	{
+		// r0c0
+		const float expR0C0 =
+			originalMatrix1(0, 0) * randomMatrix2(0, 0) + // a * j
+			originalMatrix1(0, 1) * randomMatrix2(1, 0) + // b * m
+			originalMatrix1(0, 2) * randomMatrix2(2, 0);  // c * p
+
+		EXPECT_NEAR(randomMatrix1(0, 0), expR0C0, gFloatMargin);
+
+		// r0c1
+		const float expR0C1 =
+			originalMatrix1(0, 0) * randomMatrix2(0, 1) + // a * k
+			originalMatrix1(0, 1) * randomMatrix2(1, 1) + // b * n
+			originalMatrix1(0, 2) * randomMatrix2(2, 1);  // c * q
+
+		EXPECT_NEAR(randomMatrix1(0, 1), expR0C1, gFloatMargin);
+
+		// r0c2
+		const float expR0C2 =
+			originalMatrix1(0, 0) * randomMatrix2(0, 2) + // a * l
+			originalMatrix1(0, 1) * randomMatrix2(1, 2) + // b * o
+			originalMatrix1(0, 2) * randomMatrix2(2, 2);  // c * r
+
+		EXPECT_NEAR(randomMatrix1(0, 2), expR0C2, gFloatMargin);
+	}
+
+	// row 1
+	{
+		// r1c0
+		const float expR1C0 =
+			originalMatrix1(1, 0) * randomMatrix2(0, 0) + // d * j
+			originalMatrix1(1, 1) * randomMatrix2(1, 0) + // e * m
+			originalMatrix1(1, 2) * randomMatrix2(2, 0);  // f * p
+
+		EXPECT_NEAR(randomMatrix1(1, 0), expR1C0, gFloatMargin);
+
+		// r1c1
+		const float expR1C1 =
+			originalMatrix1(1, 0) * randomMatrix2(0, 1) + // d * k
+			originalMatrix1(1, 1) * randomMatrix2(1, 1) + // e * n
+			originalMatrix1(1, 2) * randomMatrix2(2, 1);  // f * q
+
+		EXPECT_NEAR(randomMatrix1(1, 1), expR1C1, gFloatMargin);
+
+		// r1c2
+		const float expR1C2 =
+			originalMatrix1(1, 0) * randomMatrix2(0, 2) + // d * l
+			originalMatrix1(1, 1) * randomMatrix2(1, 2) + // e * o
+			originalMatrix1(1, 2) * randomMatrix2(2, 2);  // f * r
+
+		EXPECT_NEAR(randomMatrix1(1, 2), expR1C2, gFloatMargin);
+	}
+
+	// row 2
+	{
+		// r2c0
+		const float expR2C0 =
+			originalMatrix1(2, 0) * randomMatrix2(0, 0) + // g * j
+			originalMatrix1(2, 1) * randomMatrix2(1, 0) + // h * m
+			originalMatrix1(2, 2) * randomMatrix2(2, 0);  // i * p
+
+		EXPECT_NEAR(randomMatrix1(2, 0), expR2C0, gFloatMargin);
+
+		// r2c1
+		const float expR2C1 =
+			originalMatrix1(2, 0) * randomMatrix2(0, 1) + // g * k
+			originalMatrix1(2, 1) * randomMatrix2(1, 1) + // h * n
+			originalMatrix1(2, 2) * randomMatrix2(2, 1);  // i * q
+
+		EXPECT_NEAR(randomMatrix1(2, 1), expR2C1, gFloatMargin);
+
+		// r2c2
+		const float expR2C2 =
+			originalMatrix1(2, 0) * randomMatrix2(0, 2) + // g * l
+			originalMatrix1(2, 1) * randomMatrix2(1, 2) + // h * o
+			originalMatrix1(2, 2) * randomMatrix2(2, 2);  // i * r
+
+		EXPECT_NEAR(randomMatrix1(2, 2), expR2C2, gFloatMargin);
+	}
 }
 
 //***********************************************************************
@@ -251,15 +452,36 @@ TEST(Matrix3x3Tests, Transpose) {
 //***********************************************************************
 TEST(Matrix3x3Tests, Inverse) {
 	for(int i = 0; i < 50; ++i) {
-		float randomValue = testUtility.GetRandomFloat(1.0f, 10000.0f);
-		Matrix3x3 matrix1;
-		matrix1(0, 0) = randomValue;
-		matrix1(1, 1) = randomValue;
-		matrix1(2, 2) = randomValue;
-		Matrix3x3 result = matrix1.Inverse();
-		EXPECT_NEAR(result(0, 0), 1.0f / randomValue, gFloatMargin);
-		EXPECT_NEAR(result(1, 1), 1.0f / randomValue, gFloatMargin);
-		EXPECT_NEAR(result(2, 2), 1.0f / randomValue, gFloatMargin);
+
+		Matrix3x3 matrix;
+		//DirectX::XMMATRIX dxMatrix;
+
+		// Assign random values to the matrixes
+		{
+			for(int r = 0; r < 3; ++r) {
+				for(int c = 0; c < 3; ++c) {
+					const float randomValue = testUtility.GetRandomFloat(-100.0f, 100.0f);
+
+					matrix(r, c) = randomValue;
+					//dxMatrix.r[r].m128_f32[c] = randomValue;
+				}
+			}
+		}
+
+		Matrix3x3 result = matrix.Inverse();
+		DirectX::XMMATRIX dxResult = DirectX::XMMatrixInverse(nullptr, matrix);
+
+		// Test values 
+		{
+			for (int r = 0; r < 3; ++r) {
+				for (int c = 0; c < 3; ++c) {
+					const float matrixFloatValue = result(r, c);
+					const float dxFloatValue = dxResult.r[r].m128_f32[c];
+
+					EXPECT_NEAR(matrixFloatValue, dxFloatValue, gFloatMargin);
+				}
+			}
+		}
 	}
 }
 
@@ -267,8 +489,7 @@ TEST(Matrix3x3Tests, Inverse) {
 TEST(Matrix3x3Tests, CreateRotationAroundX) {
 	for(int i = 0; i < 50; ++i) {
 		float randomValue = testUtility.GetRandomFloat(-3.14f, 3.14f);
-		Matrix3x3 matrix1;
-		Matrix3x3 result = matrix1.CreateRotationAroundX(randomValue);
+		Matrix3x3 result = Matrix3x3::CreateRotationAroundX(randomValue);
 		XMMATRIX dxResult = XMMatrixRotationX(randomValue);
 		EXPECT_EQ(result, dxResult);
 	}
@@ -278,8 +499,7 @@ TEST(Matrix3x3Tests, CreateRotationAroundX) {
 TEST(Matrix3x3Tests, CreateRotationAroundY) {
 	for(int i = 0; i < 50; ++i) {
 		float randomValue = testUtility.GetRandomFloat(-3.14f, 3.14f);
-		Matrix3x3 matrix1;
-		Matrix3x3 result = matrix1.CreateRotationAroundY(randomValue);
+		Matrix3x3 result = Matrix3x3::CreateRotationAroundY(randomValue);
 		XMMATRIX dxResult = XMMatrixRotationY(randomValue);
 		EXPECT_EQ(result, dxResult);
 	}
@@ -289,8 +509,7 @@ TEST(Matrix3x3Tests, CreateRotationAroundY) {
 TEST(Matrix3x3Tests, CreateRotationAroundZ) {
 	for(int i = 0; i < 50; ++i) {
 		float randomValue = testUtility.GetRandomFloat(-3.14f, 3.14f);
-		Matrix3x3 matrix1;
-		Matrix3x3 result = matrix1.CreateRotationAroundZ(randomValue);
+		Matrix3x3 result = Matrix3x3::CreateRotationAroundZ(randomValue);
 		XMMATRIX dxResult = XMMatrixRotationZ(randomValue);
 		EXPECT_EQ(result, dxResult);
 	}
