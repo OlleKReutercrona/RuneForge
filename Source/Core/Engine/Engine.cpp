d6bac35baca1bebdefc8b0d29d1665ecbdc0b591
Olle Karlson Reutercrona <ollekreutercrona@gmail.com>
2025-09-09 22:15:22 +0200

    Added load function in engine to load config data

--- Source/Core/Engine/Engine.cpp
+++ Source/Core/Engine/Engine.cpp
@@ -4,17 +4,13 @@
 #include "frameData.h"
 
 RF::Engine::Engine(const RF::EngineCreationParams& params) {
-	// TODO: Load some of these from config file
 	RF::WindowCreationParams windowParams;
-	windowParams.width = 1920;
-	windowParams.height = 1080;
-	windowParams.title = L"RuneForge";
-	windowParams.isFullScreen = false;
-	windowParams.isResizable = true;
 	windowParams.hInstance = params.hInstance;
 	windowParams.cmdShow = params.cmdShow;
 	windowParams.engine = this;
 	windowParams.windowProc = params.windowProc;
+
+	LoadConfigFile(windowParams);
 
 	mWindow = std::make_unique<RF::Window>();
 	mWindow->Init(windowParams);
@@ -28,4 +24,51 @@ void RF::Engine::Shutdown() {}
 
 void RF::Engine::OnResize(const unsigned int width, const unsigned int height) {
 	mWindow->SetSize(width, height);
+}
+
+void RF::Engine::LoadConfigFile(RF::WindowCreationParams& windowParams) {
+	std::ifstream file("../config.json");
+	nlohmann::json json;
+	if (!file.good()) {
+		file.close();
+		return;
+	}
+
+	json = nlohmann::json::parse(file);
+
+	if (json.contains("windowSettings")) {
+		auto windowSettings = json["windowSettings"];
+
+		if (windowSettings.contains("windowSize")) {
+			auto windowSize = windowSettings["windowSize"];
+			if (windowSize.contains("width")) {
+				windowParams.width = windowSize["width"].get<unsigned int>();
+			}
+			if (windowSize.contains("height")) {
+				windowParams.height = windowSize["height"].get<unsigned int>();
+			}
+		}
+
+		if (windowSettings.contains("title")) {
+			auto title = windowSettings["title"].get<std::string>();
+			windowParams.title = std::wstring(title.begin(), title.end());
+		}
+
+		if (windowSettings.contains("startInFullscreen")) {
+			windowParams.isFullScreen = windowSettings["startInFullscreen"].get<bool>();
+		}
+
+		if (windowSettings.contains("isResizable")) {
+			windowParams.isResizable = windowSettings["isResizable"].get<bool>();
+		}
+		// TODO: Implement later when vector4 and clear color is needed
+		//if (windowSettings.contains("clearColor")) {
+		//	Vector4 clearColor;
+		//	clearColor.x = windowSettings["clearColor"]["r"].get<float>();
+		//	clearColor.y = windowSettings["clearColor"]["g"].get<float>();
+		//	clearColor.z = windowSettings["clearColor"]["b"].get<float>();
+		//	clearColor.w = windowSettings["clearColor"]["a"].get<float>();
+		//}
+	}
+
 }
--- Source/Core/pch/stdafx.h
+++ Source/Core/pch/stdafx.h
@@ -4,6 +4,10 @@
 #include <map>
 #include <vector>
 #include <memory>
+#include <fstream>
 
 // Windows
 #include <Windows.h>
+
+// External
+#include <nlohmann/json.hpp>
